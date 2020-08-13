989aa906b4e65441a3666556b976e96d6007c532
Alexander Polanco <alxpolanc@gmail.com>
2020-08-14 01:51:19 +0200

    added typescript and styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const HiComp = styled.h1`
+
+`;
+
+interface IType {
+  mode?: String;
+}
+
+const IndexPage: React.FC<IType> = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <HiComp>Hi people</HiComp>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
